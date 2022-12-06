cf4ead8ddbef40553f10098444dbd46727c65c8a
macortinas99 <matthewcortinas22@gmail.com>
2022-12-06 16:53:22 -0600

    x

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -23,7 +23,7 @@ const ProjectCards = ({ projects }) => {
                 <p className='font-code text-gold xs:text-lg sm:text-xl md:text-2xl lg:text-2xl sm:w-60% md:w-48 lg:w-62'>{project.title}</p>
                 <p className='absolute z-10 xs:hidden xs:text-base sm:text-sm text-xl text-lightBlack bg-gold xs:w-60% sm:w-50% md:w-40% lg:w-96 xs:mt-6 sm:mt-28 md:mt-12 lg:mt-16 sm:ml-6 md:ml-24 lg:ml-4 sm:p-2 md:p-2 lg:p-3 rounded-md'>
                   {/* {project.description} */}
-                  {window.innerWidth > 500 && (
+                  {window?.innerWidth > 500 && (
                     <h1 className='venueName'>
                       {project.description.length > 200 ? `${project.description.slice(0, 200)}...` : project.description}
                     </h1>
